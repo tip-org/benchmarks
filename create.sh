40e88a3f9f4cf792cb9c7f1b0f0f429b209d3306
Dan Rosén <danr@chalmers.se>
2015-02-13 18:05:59 +0100

    Almost working conversion to why3

---
+++ create.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+create() {
+    PROP=$1
+    NUM=$2
+    FILE=$3
+    DEST=$4
+    for I in $(seq -w 1 $NUM); do
+        RES=$DEST$I.mlw
+        ERR=/tmp/$I-out
+        hipspec --only=$PROP$I --print-why3 --translate-only $FILE > $RES
+        why3 prove $RES >/dev/null 2>$ERR
+        ST=$?
+        echo "$RES: ($ST)"
+        [ $ST -eq 0 ] || cat $ERR
+    done
+}
+
+(
+  mkdir -p benchmarks/prod
+  cd original/prod
+  create prop_T 50 Properties.hs ../../benchmarks/prod/prop
+) 
+(
+  mkdir -p benchmarks/isaplanner
+  cd original/isaplanner
+  create prop_ 85 Properties.hs ../../benchmarks/isaplanner/prop
+)
